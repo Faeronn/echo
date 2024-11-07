c110980f6362a45b249416172f59233559e241c8
Alexis Lelardoux <alexis.lelardoux@hotmail.fr>
2024-11-07 15:16:33 +0100

    removed print

--- src/main/java/fr/jikosoft/database/AccountsManager.java
+++ src/main/java/fr/jikosoft/database/AccountsManager.java
@@ -8,28 +8,19 @@ import fr.jikosoft.objects.Account;
 import fr.jikosoft.kernel.World;
 
 public class AccountsManager {
-    public static void getAll() {
+	public static void getAll() {
 		try {
-            List<Map<String, Object>> results = DatabaseManager.selectQuery("SELECT * FROM account");
+			List<Map<String, Object>> results = DatabaseManager.selectQuery("SELECT * FROM account");
 			
-            for (Map<String, Object> result : results) {
-                World.addAccount(new Account(
-                    (int) result.get("accountID"),
-                    ((String) result.get("username")).toLowerCase(),
-                    (String) result.get("password"),
-                    (String) result.get("nickname"),
-                    ((int) result.get("isBanned") == 1)
-                ));
-    
-                String details = String.format(
-                    "             - GUID : %d | Account : %s | Pass : %s | Pseudo : %s",
-                    result.get("accountID"),
-                    ((String) result.get("username")).toLowerCase(),
-                    result.get("password"),
-                    result.get("nickname")
-                );
-                System.out.println(details);
-            }
+			for (Map<String, Object> result : results) {
+				World.addAccount(new Account(
+					(int) result.get("accountID"),
+					((String) result.get("username")).toLowerCase(),
+					(String) result.get("password"),
+					(String) result.get("nickname"),
+					((int) result.get("isBanned") == 1)
+				));
+			}
 		}
 		catch(Exception e) {
 			System.out.println(" ! SQL ERROR: " + e.getMessage());
@@ -38,7 +29,7 @@ public class AccountsManager {
 	}
 
 	public static void update(Account account) {
-        /*
+		/*
 		PreparedStatement ps = null;
 		try {
 			ps = DatabaseManager.newTransaction("UPDATE accounts SET `nickname` = ?, `banned` = ?, `subTime` = ? WHERE `guid` = ?;");
@@ -55,8 +46,8 @@ public class AccountsManager {
 		} */
 	}
 
-    public static void load_IP_Bans()  {
-        /*
+	public static void load_IP_Bans()  {
+		/*
 		try {
 			ResultSet result = DatabaseManager.executeQuery("SELECT ip FROM ipbans;");
 			
@@ -64,7 +55,7 @@ public class AccountsManager {
 				World.addIPBan(result.getString("ip"));
 				System.out.println("             - IP : " + result.getString("ip"));
 				
-		    }
+			}
 			DatabaseManager.closeResultSet(result);
 		}
 		catch(SQLException e) {
